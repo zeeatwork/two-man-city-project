84e91c262aca5cb8e178fb8945430f79ed592d67
Zenzi Ali <zeeathome935@gmail.com>
2021-04-05 17:42:37 -0400

    Header footer and animation home page

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Layout from '../src/HOC/Layout'
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/home';
 
 const Routes = (props) => {
-  
+  return(
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path='/' />
+      </Switch>
+    </Layout>
+  )
 }
 export default Routes;
 
